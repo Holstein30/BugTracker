a08051b2b5c0addf74f423714a17da8b1ddae2a0
Dale Holstein <Holstein30@live.com>
2019-11-17 16:23:22 -0500

    Database setup - db connected

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI, () => {
+    console.log('DB connected');
+});
 
 const app = express();
 
