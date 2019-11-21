34bb20929b8c9118c5acba2fe6c2dfbd0867dbc3
Dale Holstein <Holstein30@live.com>
2019-11-21 14:10:04 -0500

    Client Side - global Footer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Header } from './global';
+import { Header, Footer } from './global';
 class App extends Component {
     render() {
         return (
             <div>
                 <Header />
                 <h1>Howdy</h1>
+                <Footer />
             </div>
         );
     }
