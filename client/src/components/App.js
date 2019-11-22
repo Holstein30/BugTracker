768e22ed624651b86297be8ccfb9850cfc07afbc
Dale Holstein <Holstein30@live.com>
2019-11-22 14:43:51 -0500

    Client Side - react router added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Header, Footer } from './global';
+import { BrowserRouter, Route } from 'react-router-dom';
 class App extends Component {
     render() {
         return (
