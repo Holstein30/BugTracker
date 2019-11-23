0987269f7f916c6dde84687b788901c74559082d
Dale Holstein <Holstein30@live.com>
2019-11-23 12:54:41 -0500

    Auth Verification - header render for login/logout

--- client/src/components/global/Header.js
+++ client/src/components/global/Header.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li>
+                        <a href="/auth/google">Login with Google</a>
+                    </li>
+                );
+            default:
+                return (
+                    <li key="2">
+                        <a href="/api/logout">Logout</a>
+                    </li>
+                );
+        }
+    }
     render() {
-        return <div>HEADER</div>;
+        return (
+            <div>
+                <ul className="right">{this.renderContent()}</ul>
+            </div>
+        );
     }
 }
 
